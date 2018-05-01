8a2310ad38567bc98c820ad060798622098ea023
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-01 05:05:13 +0200

    ..

--- scripts/pacbio_genome_assembly.sh
+++ scripts/pacbio_genome_assembly.sh
@@ -11,4 +11,4 @@
 module load bioinfo-tools
 module load canu
 
-canu -p e_faecium -d /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/pacbio_assembly genomeSize=3.5m -pacbio-raw /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/dna_raw_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.*.subreads.fastq.gz -pacbio-raw /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/dna_raw_data/PacBiom131024_200535_42174_c100563672550000001823084212221342_s1_p0.*.subreads.fastq.gz
+canu -p e_faecium -d /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/pacbio_assembly genomeSize=3.5m -pacbio-raw /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/dna_raw_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.*.subreads.fastq.gz -pacbio-raw /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/dna_raw_data/PacBio/m131024_200535_42174_c100563672550000001823084212221342_s1_p0.*.subreads.fastq.gz
