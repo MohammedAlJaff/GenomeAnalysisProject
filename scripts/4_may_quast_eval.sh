f51710187baba7bb51dd35a51327f8ec1244b5da
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-04 15:05:37 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ scripts/4_may_quast_eval.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python quast.py -o /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/quast_pacbio_assembly_eval/ /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/pacbio_assembly/e_faecium.contigs.fasta
