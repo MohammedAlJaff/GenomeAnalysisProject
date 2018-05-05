a8feddaed49e6d587276a8521047c42b0465a27d
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-05 02:13:12 +0200

    minor

--- scripts/5_may_quast_eval_with_ref.sh
+++ scripts/5_may_quast_eval_with_ref.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+module load bioinfo-tools
+module load quast
 
 quast.py -o /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/quast_eval_with_ref/ -R /home/mohamma/genome_analysis/GenomeAnalysisProject/ref_genome/ref_genome.fasta /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/pacbio_assembly/e_faecium.contigs.fasta
