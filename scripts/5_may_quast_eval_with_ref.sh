25617c8bfcbb5a882baa06237b49b8af6eb0802f
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-05 02:09:07 +0200

    shell script for quast eval + ref genome

---
+++ scripts/5_may_quast_eval_with_ref.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+quast.py -o /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/quast_eval_with_ref/ -R /home/mohamma/genome_analysis/GenomeAnalysisProject/ref_genome/ref_genome.fasta /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/pacbio_assembly/e_faecium.contigs.fasta
