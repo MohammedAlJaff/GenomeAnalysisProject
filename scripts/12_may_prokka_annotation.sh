bcfeca619be646a3ad16542725c3e2ef6c7140a2
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-12 20:17:02 +0200

    prokka script mod

--- scripts/12_may_prokka_annotation.sh
+++ scripts/12_may_prokka_annotation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 module load bioinfo-tools
 module load prokka
 
+prokka --outdir /home/mohamma/genome_analysis/genome_analysis_project_data/prokka_annotation_raw_output --prefix efaecium /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/canu_pacbio_assembly_relevant_output/efaecium.contigs.fasta
