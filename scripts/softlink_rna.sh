dc1883bd7dc5eed794d11fc2f31df60493279b95
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-16 11:05:58 +0200

    trimmed rna softlinks

---
+++ scripts/softlink_rna.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# /proj/g2018003/nobackup/private/zhang_2017/RNA_raw_data/
+
+# /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmmed_data/
+
+
+
+# rna_seq bhi-condition
+# rplicate 1
+# trim_paired_ERR1797972_pass_1.fastq.gz
+# trim_paired_ERR1797972_pass_2.fastq.gz
+ln -s /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmed_reads/trim_paired_ERR1797972* /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/rna_trimmed_reads/rnaseq_reads/bhi_condition/replicate_1/
+
+# rplicate 2
+# trim_paired_ERR1797973_pass_1.fastq.gz
+# trim_paired_ERR1797973_pass_2.fastq.gz
+ln -s /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmed_reads/trim_paired_ERR1797973* /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/rna_trimmed_reads/rnaseq_reads/bhi_condition/replicate_2/
+
+# rplicate 3
+# trim_paired_ERR1797974_pass_1.fastq.gz
+# trim_paired_ERR1797974_pass_2.fastq.gz
+ln -s /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmed_reads/trim_paired_ERR1797974* /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/rna_trimmed_reads/rnaseq_reads/bhi_condition/replicate_3/
+
+
+
+
+----- SERUM CONDITION ----
+# rna_seq SERUM-condition
+# rplicate 2
+# trim_paired_ERR1797970_pass_1.fastq.gz
+# trim_paired_ERR1797970_pass_2.fastq.gz
+ln -s /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmed_reads/trim_paired_ERR1797970* /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/rna_trimmed_reads/rnaseq_reads/serum_condition/replicate_2/
+
+# rplicate 3
+# trim_paired_ERR1797971_pass_1.fastq.gz
+# trim_paired_ERR1797971_pass_2.fastq.gz
+ln -s /proj/g2018003/nobackup/private/zhang_2017/RNA_trimmed_reads/trim_paired_ERR1797971* /home/mohamma/genome_analysis/GenomeAnalysisProject/data_links/rna_trimmed_reads/rnaseq_reads/serum_condition/replicate_3/
