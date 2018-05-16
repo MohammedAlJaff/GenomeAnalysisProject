05ee488c94cd7e84ed373c5df09ddda806d7c6a6
Mohammed Jaff <m.aljaff90@gmail.com>
2018-05-16 23:02:43 +0200

    minor

--- scripts/htseq_bhi123_serum23.sh
+++ scripts/htseq_bhi123_serum23.sh
@@ -11,5 +11,7 @@
 module load bioinfo-tools
 module load htseq
 
+cd /home/mohamma/genome_analysis/genome_analysis_project_data/htseq_bhi123_serum23_raw
+
 
 python -m HTSeq.scripts.count -f bam -r pos -i ID -t CDS /home/mohamma/genome_analysis/genome_analysis_project_data/bwa_rnaseq_raw_output/bhi_condition/mapped_replicate_1/bwa_rna_bhi_rep1_.bam /home/mohamma/genome_analysis/genome_analysis_project_data/bwa_rnaseq_raw_output/bhi_condition/mapped_replicate_2/bwa_rna_bhi_rep2_.bam home/mohamma/genome_analysis/genome_analysis_project_data/bwa_rnaseq_raw_output/bhi_condition/mapped_replicate_3/bwa_rna_bhi_rep3_.bam /home/mohamma/genome_analysis/genome_analysis_project_data/bwa_rnaseq_raw_output/serum_condition/mapped_replicate_2/bwa_rna_serum_rep2_.bam /home/mohamma/genome_analysis/genome_analysis_project_data/bwa_rnaseq_raw_output/serum_condition/mapped_replicate_3/bwa_rna_serum_rep3_.bam /home/mohamma/genome_analysis/GenomeAnalysisProject/generated_data/prokka_annotation_relevant_output/efaecium_wo_nt_seqs.gff > htseq_bhi123_serum23.txt
